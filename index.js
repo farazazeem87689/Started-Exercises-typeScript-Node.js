3777cec82c109613cff053fb05c4612721ba0edb
Faraz Azeem <farazazeem69@gmail.com>
2023-09-15 10:37:54 -0700

    Started_Exercise_TypeScript

---
+++ index.js
@@ -0,0 +1,261 @@
+"use strict";
+// let personal_message:string=("Hello Eric, would you like to learn some python today?")
+// console.log("Personal message",personal_message);
+// let personal_name:string="Faraz Azeem";
+// let lowerCase = personal_name.toLowerCase();
+// console.log("Lower case name ",lowerCase);
+// let upperCase = personal_name.toUpperCase();
+// console.log("Upper case name",upperCase);
+// let arry:string[]=personal_name.split("");
+// let titleCaseName:string=" ";
+// for(let i=0; i<arry.length; i++)
+// {
+//     titleCaseName=titleCaseName+arry[i].charAt(0).toUpperCase()+arry[i].slice(1).toLowerCase()+ " ";
+// }
+// console.log("Title Case Name", titleCaseName);
+// console.log('Albert Einstein once said,"A person who never made a mistake never tried anything new"');
+// let famous_Qout:string='Albert Einstein';
+// // let message:string =' ${famous_Qout} said,"A person who never made a mistake never tried anything new."'
+// console.log(` ${famous_Qout} said,A person who never made a mistake never tried anything new`)
+// let nam:string="\t      Faraz azeem      \n"
+// console.log("Some Whitespace name"nam);
+// console.log(nam.trim())
+// let addition:number=4+4;
+// console.log("Addition Number",addition)
+// let Subtraction:number=16-8;
+// console.log("Subtraction Number",Subtraction)
+// let multiplication:number=4*2;
+// console.log("Multiplication Number",multiplication)
+// let divison:number=64/8;
+// console.log("Divison Number",divison)
+// for(let i=0; i<4; i++)
+// {
+//     console.log(5+3)
+// }
+// let favorite_number:number=87689;
+// let num=`favorite number: ${favorite_number}`;
+// console.log(num);
+// var Name="faraz azeem";
+// var date="08-09-2023";
+// console.log(`name ${Name}:Date ${date}`);
+// let array:string[]=["faraz","Faran","Aness","Farrukh","shamoon"]
+// for (let i=0; i<array.length; i++)
+// {
+//     console.log(array[i]);
+// }
+// console.log(array[0])
+// console.log(array[1])
+// console.log(array[2])
+// console.log(array[3])
+// console.log(array[4])
+// let array:string[]=["faraz","Faran","Aness","Farrukh","shamoon"]
+// for (let i=0; i<array.length; i++)
+// {
+//     console.log(`hello ${array[i]} i hope you are doing well`);
+// }
+// let favoriteTransportation:string[]=["suzuki motorcycle","Honda car"]
+// for (let i=0; i<favoriteTransportation.length; i++)
+// {
+//     console.log(`I would like to own a ${favoriteTransportation[i]}`)
+// }
+// let guestList:string[]=["faraz","faran","farrukh"]
+// guestList.forEach((invitaion)=>{
+//     console.log(`Mr. ${invitation} i would like to invite you to attend the dinner which for the fellows`)
+// })
+// let GuestList:string[]=["faraz","faran","farrukh","Aness"]
+// console.log(GuestList)
+// let PersonNotCome:string[]=["Anees"]
+// console.log("Person not to come" , PersonNotCome);
+// GuestList.splice(3,1,"Azam")
+// console.log("Modify Name",GuestList);
+// for(let i=0; i<GuestList.length; i++)
+// {
+//     console.log(`Mr.${GuestList[i]} I am very gald you to invite on my dinner`)
+// }
+// let GuestList:string[]=["faraz","faran","farrukh","Aness"]
+// console.log("Guest List",GuestList);
+// GuestList.unshift("Azam");
+// GuestList.splice(GuestList.length/2,0,"kashif")
+// GuestList.push("Shanza")
+// GuestList.forEach((invitation)=>(
+//     console.log(`Mr.${invitation} i am gald to invite you on dinner`)
+// ))
+// console.log(GuestList);
+// let favoriteLocation:string[]=["America","United Kingdom","pakistan","Canada","Isreal"]
+// console.log("Original order")
+// console.log(favoriteLocation);
+// console.log("\n  Alphabetical order")
+// favoriteLocation.sort()
+// console.log(favoriteLocation);
+// console.log("Original order")
+// console.log(favoriteLocation);
+// console.log("\n Reverse Alphabetical order")
+// favoriteLocation.sort().reverse()
+// console.log(favoriteLocation);
+// console.log("Original order")
+// console.log(favoriteLocation);
+// favoriteLocation.reverse()
+// console.log("Reverse Order",favoriteLocation);
+// favoriteLocation.sort()
+// console.log("Original Order",favoriteLocation);
+// favoriteLocation.reverse()
+// console.log(favoriteLocation)
+// let DinnerGuestList:string[]=["faraz","faran","farrukh","Anees"]
+// console.log("No of Guest is invited:",DinnerGuestList.length)
+// let country:string[]=["pakistan","india","America","United Kingdom","Isreal"]
+// for(let i=0; i<country.length; i++)
+// {
+// //     console.log("Country Name list",country[i])
+// }
+// let motorcycle={band:"honda",modelType:"CD 70", year:"2023"}
+// console.log(motorcycle)
+// let identification=[{Name:"faraz",city:"sahiwal",Province:"punjab",phone:"098765453"},
+// {Name:"faran",city:"sahiwal",Province:"punjab",phone:"098765453"},
+// {Name:"farrukh",city:"sahiwal",Province:"punjab",phone:"098765453"}
+// ]
+// console.log(identification)
+// let intentionError:string[]=["faraz","faran","farrukh","Aness","Azam","kashif"]
+// console.log(`intention Error: ${intentionError[3]}`)
+// console.log(`intention Error: ${intentionError[5]}`)
+// let car='subaru';
+// console.log("is car=='subaru'? is | predict is ture")
+// console.log(car=='subaru');
+// console.log("is car=='honda'? | predict is false")
+// console.log(car=='honda');
+// console.log("is car==='subaru'? | my predict is true")
+// console.log(car==='subaru');
+// console.log("is car==='honda'?| predict is false")
+// console.log(car==='subaru');
+// console.log(" is car !== subaru ?| predict is false")
+// console.log(car=='subaru');
+// console.log("is car !==subaru ? | predict is true")
+// console.log(car=='subaru')
+// let alien_color="green";
+// if (alien_color=='green')
+// {
+//     console.log("The player just earned 5 point")
+// }
+// else
+// {
+//     console.log("That fails will have no output")
+// }
+// let alien_color="green";
+// if (alien_color=='green')
+// {
+//     console.log("The player just earned 5 point")
+// }
+// else
+// {
+//     console.log("The player just earned 10 points")
+// }
+// let alienColor='green';
+// if (alienColor=='green')
+// {
+//     console.log("The player earned 5 points")
+// }
+// else if (alienColor=='yellow')
+// {
+//     console.log("The player earned 10 points")
+// }
+// else if (alienColor=='red')
+// {
+//     console.log("The player earned 15 points")
+// }
+// else
+// {
+//     console.log("Blank")
+// }
+// let age:number=80;
+// if(age>0 && age<2)
+// {
+//     console.log("The person is baby")
+// }
+// else if (age>=2 && age<4)
+// {
+//     console.log("The person is toddler")
+// }
+// else if (age<=4 && age<13)
+// {
+//     console.log("The person is kid")
+// }
+// else if (age<=13 && age<20)
+// {
+//     console.log("The person is teenager")
+// }
+// else if (age<=20 && age<65)
+// {
+//     console.log("The person is adult")
+// }
+// else if (age>=65)
+// {
+//     console.log("The person is elder")
+// }
+// else
+// {
+// console.log("Error")
+// }
+// let fruits:string[]=["banana","apple","mango","dates","watermelon"]
+// let favorite_fruits:string[]=["watermelon","apple","mango"]
+// for (let i=0; i<fruits.length; i++)
+// {
+//     if(fruits.includes(favorite_fruits[i]))
+//     {
+//         console.log(`you are like ${favorite_fruits[i]}`)
+//     }
+// }
+// let helloAdmin:string[]=["admin","IT officer","coordinator","OFA","Accountant"]
+// let helloPerson:string[]=["admin"]
+// for (let i=0; i<helloAdmin.length; i++)
+// {
+//     if (helloAdmin.includes(helloPerson[i]))
+//     {
+//         console.log(`Hello: ${helloPerson[i]} would you like to see a status report`)
+//     }
+//         else
+//         {
+//             console.log(`hello:${helloAdmin[i]} thank you for logging in again`)
+//         }
+//     }
+// let current_user:string[]=["faraz","faran","farrukh","Azeem","Anees"]
+// let new_user:string[]=["faraz","Kashif","Saqib","Azeem","faran"]
+// for (let i = 0 ; i <new_user.length; i++ )
+// {
+// if (new_user.includes(current_user[i]))
+// {
+//     console.log(`The person will need to enter a new username`)
+// }
+// else{
+//     console.log(`The username is available`)
+// }
+// }
+// let num: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let number of num)
+//  {
+//   let ordinal_num = "";
+//   if (number === 1) 
+//   {
+//     ordinal_num = "st";
+//   } 
+//   else if (number === 2) 
+//   {
+//     ordinal_num = "nd";
+//   } 
+//   else if (number === 3)
+//    {
+//     ordinal_num = "rd";
+//   }
+//    else 
+//    {
+//     ordinal_num = "th";
+//   }
+//   console.log(`${number}${ordinal_num}`);
+// }
+let favoritePizzas = ["Pepperoni", "Margherita", "Supreme"];
+// Print the names of each pizza
+console.log("My favorite pizzas are:", favoritePizzas);
+// Print a sentence for each pizza
+// console.log("\nDescriptive statements about my favorite pizzas:");
+for (let pizza of favoritePizzas) {
+    console.log(`\n I like ${pizza} pizza.`);
+}
+console.log("\nI really love pizza!");
